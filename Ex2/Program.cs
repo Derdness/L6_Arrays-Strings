8c2e4567f689bf03cd92a6123ed8f8da5344a167
Александр Романов <derdness@yandex.ru>
2024-01-01 20:58:44 +0700

    Решение задачи 2 по теме Массивы и строки
    Задача 2: Задайте строку, содержащую латинские
    буквы в обоих регистрах. Сформируйте строку,
    в которой все заглавные буквы заменены на строчные.

---
+++ Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 2: Задайте строку, содержащую латинские 
+буквы в обоих регистрах. Сформируйте строку, 
+в которой все заглавные буквы заменены на строчные.
+*/
+
+string original = "A qUiCk BrOwN fOx JuMpS oVeR a LaZy DoG.";
+string capital = String.Empty;
+
+//Записываем в новую строку первую с символами в нижнем регистре
+capital = original.ToLower();
+
+Console.WriteLine(original);
+Console.WriteLine(capital);
