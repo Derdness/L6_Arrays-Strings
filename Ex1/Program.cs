3dce1910ebe2f520a1a2747a4ed3e494f1e1ca02
Александр Романов <derdness@yandex.ru>
2024-01-01 20:49:23 +0700

    Решение задачи 1 по теме Массивы и строки
    Задача 1: Задайте двумерный массив символов
    (тип char [,]). Создать строку из символов этого массива.

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -2,4 +2,19 @@
 (тип char [,]). Создать строку из символов этого массива.
 */
 
+using System.Text;
+//Инициализируем двумерный массив символов
 char[,] arr = {{'H','e','l','l','o'},{'w','o','r','l','d'}};
+//Класс для обработки строк
+StringBuilder sb = new StringBuilder();//Класс для обработки строк
+
+string result = String.Empty;
+//Пройдём по каждому символу в заданном массиве и
+//добавим его к концу построителя? строк
+foreach(char letter in arr)
+{
+    sb.Append(letter);
+}
+//Получим финальную строку
+result = sb.ToString();
+Console.WriteLine(result);
